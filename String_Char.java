1a7b29bcd17219a50f2db7e8dcab5f5edb64a3c3
Eniya G <rg066234@gmail.com>
2025-08-09 12:16:00 +0530

    Daily update: <your message>

---
+++ String_Char.java
@@ -0,0 +1,10 @@
+public class String_Char{
+    public static void main(String[]args){
+        String word="Listen";
+        char[] s=word.toCharArray();
+        for(char c:s){
+            System.out.println(c);
+        }
+    }
+
+}
