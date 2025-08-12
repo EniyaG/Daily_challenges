5d9f44cd9c306146531c3f3fbb52edb3c05a04cd
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-12 07:06:29 +0530

    Add files via upload

---
+++ Reverse_Array.java
@@ -0,0 +1,10 @@
+package daily_pracrice;
+import java.util.Arrays;
+public class Reverse_Array {
+	public static void main(String[]args) {
+	int[] a= {9,4,61,8,14,5};
+    Arrays.sort(a);
+    System.out.println(a);
+
+}
+}
