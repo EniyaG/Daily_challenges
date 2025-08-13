3d138b8020c945db3b69e53479d1fff1cb75e4d9
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-13 07:20:15 +0530

    Add files via upload

---
+++ ReverseLetters.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+public class ReverseLetters {
+    public static void main(String[]args){
+    char[] letter={'f','r','a','c'};
+    Arrays.sort(letter);
+    System.out.println(letter);
+    
+}
+}
---
+++ ReverseWords.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+public class ReverseWords {
+    public static void main(String[]args){
+    String[] words={"Eniya","Ramya","Arjun"};
+    Arrays.sort(words);
+    System.out.println(Arrays.toString(words));
+
+    
+}
+}
