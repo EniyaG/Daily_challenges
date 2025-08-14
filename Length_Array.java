6a4ab90fa85ca27a4a2a12e161d2574734bf1975
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-14 07:03:28 +0530

    Add files via upload

---
+++ Length_Array.java
@@ -0,0 +1,9 @@
+public class Length_Array {
+    public static void main(String []args){
+        int []arr={1,4,5,6,7,8};
+        
+            System.out.println("lenth of an Array:"+arr.length);
+        }
+    }
+    
+
