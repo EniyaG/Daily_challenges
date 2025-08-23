784e002afdfccf27c3bca34f18641d5daac1d931
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-23 06:55:50 +0530

    Add files via upload

---
+++ Factorial_Loop.java
@@ -0,0 +1,11 @@
+public class Factorial_Loop {
+    public static void main(String[] args) {
+        int n=10;
+        int fact=1;
+        for(int i=1;i<10;i++){
+            fact=fact*i;
+            System.out.println("fact:"+fact);
+        }
+    }}
+    
+
