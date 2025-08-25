598a96f1df5f5312aa3859cf82b48c7f1fecf64e
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-25 06:33:41 +0530

    Finding Minimum and maximum number from the array without using inbuilt functions. first declare the min and max as 0 later using for loop we find the min and max

---
+++ Min_max_loop.java
@@ -0,0 +1,21 @@
+public class Min_max_loop {
+    public static void main(String[]args){
+        int []arr={1,4,5,6,7,8,9};
+        int min=arr[0];
+        int max=arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if(arr[i]<min){
+                min=arr[i];
+            }
+            if(arr[i]>max){
+                max=arr[i];
+
+            }
+        }
+            System.out.println("minimum"+min);
+            System.out.println("maximum"+max);
+
+        
+    }
+    
+}
