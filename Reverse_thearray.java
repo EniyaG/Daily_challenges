055883996df2fb4585491805f00a953805b9012f
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-26 06:25:47 +0530

    Reversing the array using loop

---
+++ Reverse_thearray.java
@@ -0,0 +1,13 @@
+public class Reverse_thearray {
+    public static void main(String[] args) {
+        
+    
+    int []a={1,6,7,3,5};
+    for(int i=a.length-1;i>=0;i--){
+    
+        System.out.print(a[i]);
+
+    } 
+    
+}
+}
