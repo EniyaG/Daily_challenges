e3ac6611c1a3cec16df2ba92180ecd86b3017df3
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-26 15:19:01 +0530

    Removing all the whitespaces from the string
    In Removing all the white spaces can use "replaceAll()" to remove all the spaces in the string
    replace()-- it is also amethod used to replace the whitespace but here we need to specify the places.
    trim()--it is also a method used to remove the empty spaces from starting and at the end "not in the middle".

---
+++ Removing_whitespace.java
@@ -0,0 +1,8 @@
+public class Removing_whitespace {
+    public static void main(String[]args){
+        String str="hello Lets remove White space";
+        String ee=str.replaceAll("//s+","");
+        System.out.println(ee);
+    }
+    
+}
