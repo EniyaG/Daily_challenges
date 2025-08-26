fc8599b34a84a47bf2885348708080603eac712f
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-08-26 12:15:25 +0530

    Swapping the numbers without using temporary variable (by usi g addition methods)

---
+++ Swap_Notemp.java
@@ -0,0 +1,11 @@
+public class Swap_Notemp{
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        a=a+b;//30
+        b=a-b;//10
+        a=a-b;//20
+        System.out.println("the value of A is : " +a+ "the value of B is: " +b);
+
+    }
+}
