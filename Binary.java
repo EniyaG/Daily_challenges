8842af691e2e7e47690eda7fedc35369209a461c
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-09-02 20:56:52 +0530

    Add files via upload

---
+++ Binary.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+public class Binary {
+    public static void main(String[]args){
+        TreeSet<Integer>bst=new TreeSet<>();
+        bst.add(2);
+        bst.add(5);
+        bst.add(66);
+        System.out.println(bst);
+    }
+    
+}
+
