4e9700b6325d9a20387d5d46b22aeb7ebec2759e
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-09-03 21:48:28 +0530

    It is one of the binary search program ,here we use trrMap concept(inside the treeMap we should store key value pair )It is present inside the util package

---
+++ binary_map.java
@@ -0,0 +1,16 @@
+
+import java.util.TreeMap;
+
+public class binary_map {
+    public static void main(String[] args) {
+        TreeMap<Integer,String>mm=new TreeMap<>();
+        mm.put(1,"apple");
+        mm.put(4,"asdf");
+        mm.put(5,"sdfg");
+        System.out.println("the binary treemap are:"+mm);
+        
+
+        
+    }
+    
+}
