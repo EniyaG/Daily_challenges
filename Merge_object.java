5576e8b42cc7016d27e444a151f9c556c9d969cd
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-09-07 07:34:21 +0530

    Add files via upload
    Mergesort-merge sort works on divife,conquer,merge .Can handle the merge sort using Arrays.sort()--it internally work as timSort,timsort is inspired by merge sort

---
+++ Merge_object.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+public class Merge_object {
+    public static void main(String[] args) {
+        Integer a[]={1,2,3,4,5,10,9,8,7,6};
+        System.out.println("The original arrays are:");
+        System.out.println(Arrays.toString(a));
+Arrays.sort(a);
+        System.out.println("The sorted arrays are:"+Arrays.toString(a));        
+    }}
+    
+
