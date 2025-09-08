ef21e3e7fa08ee27568c00db5108dce2a76de961
Eniya G <163950497+EniyaG@users.noreply.github.com>
2025-09-08 18:57:47 +0530

    Add files via upload
    Sum of an array

---
+++ sum_array.java
@@ -0,0 +1,12 @@
+public class sum_array {
+    public static void main(String[]args){
+        int a[]={4,6,7,8,9};
+        int sum=0;
+        for(int numbers:a){
+            sum+=numbers;
+
+        }
+        System.out.println(sum);
+    }
+    
+}
